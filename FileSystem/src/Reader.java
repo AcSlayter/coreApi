a95bc32fee59fce6e18c4749325beba031c73d77
Aaron <aaronbcain@gmial.com>
2018-08-23 21:52:28 -0700

    Adding example case files

---
+++ FileSystem/src/Reader.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+public class Reader {
+
+    public static void main(String[] args) {
+
+        try {
+
+            File file = new File("test.xml");
+            JAXBContext jaxbContext = JAXBContext.newInstance(Customer.class);
+
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            Customer customer = (Customer) jaxbUnmarshaller.unmarshal(file);
+            System.out.println(customer);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
